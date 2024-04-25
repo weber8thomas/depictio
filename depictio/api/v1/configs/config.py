da7d3550c2edcd924a2631c66848868230804a5b
Thomas Weber <weber8thomas@gmail.com>
2024-04-26 00:08:52 +0200

    config.py update for example

--- depictio/api/v1/configs/config.py
+++ depictio/api/v1/configs/config.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import logging
+
 
 from depictio.api.v1.configs.settings_models import Settings
 from depictio.api.v1.utils import get_config, validate_config
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s")
-logger = logging.getLogger("depictio")
 
 # Settings
 # Overwrite priority: environment variables (.env) > config file (.yaml) > default values
@@ -14,4 +17,13 @@ API_BASE_URL = f"http://{settings.fastapi.service_name}:{settings.fastapi.port}"
 MONGODB_URL = f"mongodb://{settings.mongodb.service_name}:{settings.mongodb.port}/"
 TOKEN = settings.auth.tmp_token
 
+# FIXME: the token above is not used by Dash frontend, only by FastAPI backend
+TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjJhY2FiNDQ3ZDk5OGNkODg5Njc4YzEiLCJleHAiOjE3OTE4NDA0MzZ9.CD-n5cvmn-..."
+
+logger = logging.getLogger("depictio")
+
+logger.info(f"API_BASE_URL: {API_BASE_URL}")
+logger.info(f"MONGODB_URL: {MONGODB_URL}")
+logger.info(f"TOKEN: {TOKEN}")
+logger.info(f"Token from .env: {settings.auth.tmp_token}")
 
